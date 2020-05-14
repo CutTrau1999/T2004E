f6e78c2ee256ce9050150c3cfa9c78e53d15e9f7
CutTrau1999 <64960478+CutTrau1999@users.noreply.github.com>
2020-05-14 16:08:26 +0700

    nop Assignment 4

---
+++ chiahetcho3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+int n,i;
+printf("Nhap so:\n");
+scanf(" %d",&n);
+
+for(int i=0; i<=n;i++){ 
+
+if(i%3==0){
+	
+	
+    printf(" So chia het cho 3: %d\n",i);
+    
+}
+}
+}
+
+
+
---
+++ tinhtong.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	printf("S=1+2+3+4+...+1000 =");
+	int i,s;
+	i=1;
+	for(i;i<=1000;i++){
+		s+=i;}
+		printf(" %d",s);
+		}
